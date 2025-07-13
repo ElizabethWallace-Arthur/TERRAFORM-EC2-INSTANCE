d2ea6dce1ec3a7a2453fb8d3b1969e310f7ae29b
Elizabeth Wallace Arthur <elizabethw.arthur@gmail.com>
2025-07-13 21:42:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@
 resource "aws_instance" "firstEC2" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-  key_name      = var.key_pair_name
 
   tags = {
     Name = "MyFirstEC2"
